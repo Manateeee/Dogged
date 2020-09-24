805bdad1b3b49765d7f9944d6a24366e5b228e44
fazerkex <71411571+fazerkex@users.noreply.github.com>
2020-09-25 00:00:17 +0300

    somechanges

---
+++ src/InfoUI.py
@@ -0,0 +1,2 @@
+class InfoUI:
+    pass
--- src/TileMap.py
+++ src/TileMap.py
@@ -1,2 +1,31 @@
 class TileMap:
+<<<<<<< Updated upstream
     
+=======
+    def __init__(self):
+        self.tileset = TileSet("Utils\Pictures\TileMap.png", (255, 0, 255), 32, 32)
+        self.tileset.add_tile("path", 0, 0)
+        self.tileset.add_tile("mud", 32, 0)
+
+        self.tile_list = list()
+
+        self.tile_list.append("path")
+        self.tile_list.append("mud")
+       
+        self.height = 50
+        self.width = 50
+    
+    def render(self, screen):
+
+
+        #zweidarray =  [][5]
+        tilename = self.tile_list[0]
+        tile = self.tileset.get_tile(tilename)
+        for xpos in range(0,1200,self.width):
+            for ypos in range(0,800,self.height):
+                screen.blit(self.tileset.image, (xpos,ypos), tile.rect)
+        
+
+        
+        
+>>>>>>> Stashed changes
