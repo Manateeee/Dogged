58ddba8068ab700f59f9a2dfaacd1703935a3cf1
Thalthy <tobias.vollath@gmail.com>
2020-09-30 00:51:02 +0200

    UI Button bar

--- src/UI.py
+++ src/UI.py
@@ -1,2 +1,18 @@
 class UI:
-    
+    import pygame
+
+    pygame.init()
+    screen = pygame.display.set_mode((1200,800))
+
+    finished = False # 0 < 10 --> True / 10<10 --> False
+
+    while finished == False:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                finished = True
+
+        rectButtonBar = pygame.Rect(0,750,1200,50) # x,y,width,height
+
+        color = (162,155,108) # R,G,B
+        pygame.draw.rect(screen,color,rectButtonBar)
+        pygame.display.flip()
