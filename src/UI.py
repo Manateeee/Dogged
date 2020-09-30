3696cce49253b726e84f2f3faf8b4dac65865201
Thalthy <tobias.vollath@gmail.com>
2020-09-30 21:29:46 +0200

    Update UI.py
    Versuchung eines Labels

--- src/UI.py
+++ src/UI.py
@@ -4,7 +4,7 @@ class UI:
     pygame.init()
     screen = pygame.display.set_mode((1200,800))
 
-    finished = False # 0 < 10 --> True / 10<10 --> False
+    finished = False # 0 < 10 --> True / 10 < 10 --> False
 
     while finished == False:
         for event in pygame.event.get():
@@ -16,3 +16,14 @@ class UI:
         color = (162,155,108) # R,G,B
         pygame.draw.rect(screen,color,rectButtonBar)
         pygame.display.flip()
+
+def Text(): #Noch am arbeiten
+    import pygame
+
+    pygame.Display.fill(white)
+
+    rectButtonBar = pygame.Rect(0,450,1200,50) # x,y,width,height
+    color = (162,155,108) # R,G,B
+    pygame.draw.rect(screen,color,rectButtonBar)
+
+    pygame.display.update()
