370f4790cc8ebe51aae21d4ce86a51b32d270b43
fazerkex <71411571+fazerkex@users.noreply.github.com>
2020-10-01 19:10:46 +0300

    Enemy
    some Constructor and Methods

--- src/Enemies.py
+++ src/Enemies.py
@@ -1,2 +1,11 @@
+from EnemyType import EnemyType
+
 class Enemies:
     
+    def __init__(self):
+        self.Enemy = EnemyType()
+        self.Enemy.add_Enemy("Peepo",0 , 5 , 0 , 0)
+
+        #List of the EnemyTypes
+        self.Enemy_list = list()
+        self.Enemy_list.append("Peepo")
---
+++ src/EnemyWave.py
@@ -0,0 +1,7 @@
+from Enemies import Enemies
+
+class EnemyWave:
+    WaveList = list()
+    def __init__(self, start_x, start_y):
+        self.start_x = start_x
+        self.start_y = start_y
