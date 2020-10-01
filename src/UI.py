27bc55265a41e6919f6da894a388326d3756a956
fazerkex <71411571+fazerkex@users.noreply.github.com>
2020-10-01 21:20:48 +0300

    Fixes
    Fixes

--- src/UI.py
+++ src/UI.py
@@ -1,11 +1,12 @@
+import pygame
 class UI:
-    import pygame
+    
 
     pygame.init()
     screen = pygame.display.set_mode((1200,800))
 
     finished = False # 0 < 10 --> True / 10 < 10 --> False
-
+    screen.fill((255,255,255))
     while finished == False:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,13 +18,13 @@ class UI:
         pygame.draw.rect(screen,color,rectButtonBar)
         pygame.display.flip()
 
-def Text(): #Noch am arbeiten
-    import pygame
+    def Text(self): #Noch am arbeiten
+        import pygame
 
-    pygame.Display.fill(white)
+        
 
-    rectButtonBar = pygame.Rect(0,450,1200,50) # x,y,width,height
-    color = (162,155,108) # R,G,B
-    pygame.draw.rect(screen,color,rectButtonBar)
+        rectButtonBar = pygame.Rect(0,450,1200,50) # x,y,width,height
+        color = (162,155,108) # R,G,B
+        pygame.draw.rect(self.screen,color,rectButtonBar)
 
-    pygame.display.update()
+        pygame.display.update()
